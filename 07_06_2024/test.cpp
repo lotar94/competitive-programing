254efc578e043e40706a0bb732cbe7105dd8ece8
Lotar <lotar.94@gmail.com>
2024-06-08 01:58:43 -0400

    more resource and problems

--- 07_06_2024/test.cpp
+++ 07_06_2024/test.cpp
@@ -4,15 +4,23 @@ using namespace std;
 
 int main()
 {
-    int w;
-    cin >> w;
+    int array[8] = {-1, 2, 4, -3, 5, 2, -5, 2};
+    int best = 0;
+    int n = 8;
 
-    if (w != 2 && (w % 2) == 0)
+    for (int a = 0; a < n; a++)
     {
-        cout << "YES";
+        for (int b = a; b < n; b++)
+        {
+
+            int sum = 0;
+            for (int k = a; k <= b; k++)
+            {
+                cout << array[k];
+                sum += array[k];
+            }
+            best = max(best, sum);
+        }
     }
-    else
-    {
-        cout << "NO";
-    }
+    cout << best << "\n";
 };
--- Problemt_Set/800/Watermelon.py
+++
@@ -1,6 +0,0 @@
-x = input()
-y = int(x) % 2
-if int(x) != 2 and y ==  0:
-    print("YES")
-else:
-    print("NO")
