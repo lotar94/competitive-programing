055aa419ae667fc148a0944ca09bd84e8e706eae
Lotar <lotar.94@gmail.com>
2024-06-15 00:08:14 -0400

    solve problema spell-check

--- 14_06_2024/SpellCheck.cpp
+++ 14_06_2024/SpellCheck.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -23,10 +24,34 @@ int main()
     {
         if (names[i].length() == 5)
         {
+            string aux = names[i];
+            char letraT = 'T';
+            char letrai = 'i';
+            char letram = 'm';
+            char letrau = 'u';
+            char letrar = 'r';
+            bool contieneT = find(aux.begin(), aux.end(), letraT) != aux.end();
+            bool contienei = find(aux.begin(), aux.end(), letrai) != aux.end();
+            bool contienem = find(aux.begin(), aux.end(), letram) != aux.end();
+            bool contieneu = find(aux.begin(), aux.end(), letrau) != aux.end();
+            bool contiener = find(aux.begin(), aux.end(), letrar) != aux.end();
+            if (contieneT == true && contienei == true && contienem == true && contieneu == true && contiener == true)
+            {
+                response[i] = "YES";
+            }
+            else
+            {
+                response[i] = "NO";
+            }
         }
         else
         {
             response[i] = "NO";
         }
+    }
+
+    for (size_t i = 0; i < testCases; i++)
+    {
+        cout << string(response[i]) << "\n";
     }
 }
