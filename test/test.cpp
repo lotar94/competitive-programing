3fc4073ec914e8f7f2a9b0872f5e24195ebc946c
Lotar <lotar.94@gmail.com>
2024-06-27 09:00:11 -0400

    codeforces problem CalculatingFuntion

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-
+// test cambios
 int main()
 {
+    // lista de los valores utilizar
     int array[8] = {-1, 2, 4, -3, 5, 2, -5, 2};
-    int best = 0;
-    int n = 8;
+    int best = 0; // se guardara la mejor suma
+    int n = 8;    // numero de elementos a recorrer
 
+    // Recorremos la lista completa
     for (int a = 0; a < n; a++)
     {
+        // recorremos
         for (int b = a; b < n; b++)
         {
-
             int sum = 0;
             for (int k = a; k <= b; k++)
             {
